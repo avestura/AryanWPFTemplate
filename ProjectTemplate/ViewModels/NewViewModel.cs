bb304f872369e5609bebe523f8211aaa0e702b14
Aryan <aryanebrahimpour@gmail.com>
2018-03-26 10:43:30 +0430

    Add INotifyPropertyChanged implemention to ViewModel example

--- ProjectTemplate/ViewModels/NewViewModel.cs
+++ ProjectTemplate/ViewModels/NewViewModel.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectTemplate.ViewModels
 {
-    public class NewViewModel
+    public class NewViewModel : INotifyPropertyChanged
     {
         public string Name { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
     }
 }
