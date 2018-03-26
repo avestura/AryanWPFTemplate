da6ef2c12b9767f828c446feb46e0cf467bc73a3
Aryan <aryanebrahimpour@gmail.com>
2018-03-26 10:06:12 +0430

    Add project files.

---
+++ ProjectTemplate/ViewModels/NewViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectTemplate.ViewModels
+{
+    public class NewViewModel
+    {
+        public string Name { get; set; }
+    }
+}
