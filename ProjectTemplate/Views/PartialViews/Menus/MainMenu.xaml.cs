fc705744f96c94b2711325a435a2c75646d7200a
Aryan <aryanebrahimpour@gmail.com>
2018-04-02 11:26:38 +0430

    Updated Menu Partial View

--- ProjectTemplate/Views/PartialViews/Menus/MainMenu.xaml.cs
+++ ProjectTemplate/Views/PartialViews/Menus/MainMenu.xaml.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace ProjectTemplate.Views.PartialViews.Menus
 {
-    public partial class MainMenu : Menu
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : UserControl
     {
-
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
     }
 }
