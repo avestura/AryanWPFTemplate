f75c666c1c8ee6b13d66c8258cbb0f36b985f78e
Aryan <aryanebrahimpour@gmail.com>
2018-04-15 22:45:30 +0430

    Behavior Model added

---
+++ ProjectTemplate/Views/Behaviors/BlinkBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace ProjectTemplate.Views.Behaviors
+{
+    public class BlinkBehavior : Behavior<UIElement>
+    {
+
+        protected override void OnAttached()
+        {
+            AssociatedObject.BlinkEasing();
+        }
+
+    }
+}
