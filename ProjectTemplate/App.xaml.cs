1b584ea274f95e0d4df0ba5396c36bf925fbb2ad
Aryan <aryanebrahimpour@gmail.com>
2018-04-15 22:45:53 +0430

    Add doc comment to App.xaml.cs

--- ProjectTemplate/App.xaml.cs
+++ ProjectTemplate/App.xaml.cs
@@ -13,8 +13,14 @@ namespace ProjectTemplate
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Returnes current app as an <see cref="App"/> instead of an <see cref="Application"/>
+        /// </summary>
         public static App CurrentApp => (App)Current;
 
+        /// <summary>
+        /// Returens current <see cref="Configuration"/> of application
+        /// </summary>
         public Configuration Configuration { get; set; }
 
     }
